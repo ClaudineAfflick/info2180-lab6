3bd14a2f7dd55db5d86ad27f4bc82058d6353404
ClaudineAfflick <claudine.afflick@mymona.uwi.edu>
2018-11-14 03:51:59 +0000

    excercise 1

---
+++ js.js
@@ -0,0 +1,33 @@
+window.onload=function(){
+    /*var textbox= document.getElementById("searchbox").value;*/
+    var search=document.getElementById("search");
+    var request;
+    var Result=document.getElementById("p1");
+    
+    search.addEventListener("click",function(){
+        var textbox= document.getElementById("searchbox").value;
+        request = new XMLHttpRequest();
+        var definitionURL= 'request.php?q='+textbox;
+        request.onreadystatechange=function(){
+            if (request.readyState===request.DONE){
+                if (request.status===200){
+                    var response=request.responseText;
+                    console.log(request.responseText);
+                    Result.innerHTML=response;
+                    
+                    //alert(response);
+                }
+                if(request.status===404){
+                    console.log("Result not found.");
+                alert('NOT FOUND');
+                }
+            }
+           
+        };
+        request.open('GET',definitionURL,true);
+        request.send();
+    
+    });
+    
+    
+};
